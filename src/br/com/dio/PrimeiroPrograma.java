627640714df53997cbc3f93638faa833ca149fac
amercador <amercador@hotmail.com>
2021-09-10 22:36:11 -0300

    Segundo Commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,8 +1,13 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
+
+        Gato gato = new Gato("Miau", "preto", 7);
+        System.out.println(gato);
 
         System.out.println("Hello World!");
     }
